330e40e4da45eef682593e2ac9010bb53993c31d
Vikash951 <101057544+Vikash951@users.noreply.github.com>
2023-07-01 16:27:40 +0530

    app.js added

---
+++ app.js
@@ -0,0 +1,187 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const mongoose = require('mongoose');
+const _ = require("lodash");
+
+//connection url
+const url = 'mongodb+srv://vikky0403:vikash1234@vikash.npbbqjg.mongodb.net/todolistDB?retryWrites=true&w=majority';
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:',error);
+});
+
+const itemsSchema = {
+    name:String
+};
+
+const Item = mongoose.model("Item" , itemsSchema)
+
+const item1 =new Item({
+    name:"Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems =[item1 , item2 , item3];
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model("List" , listSchema);
+
+// Item.insertMany(defaultItems)
+//  .then(function(){
+//     console.log("Data inserted")
+//  })
+//  .catch(function(error){
+//     console.log(error)
+//  })
+
+ 
+    
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.get("/" , function(req , res){
+
+    Item.find({})
+        .then(foundItems =>{
+            if(foundItems.length === 0){
+                return Item.insertMany(defaultItems);
+            }
+            else{
+                return foundItems;
+            }
+        })
+        .then(foundItems=>{
+            res.render("list" ,{listTitle : "Today" , newListItems : foundItems});
+        })
+        .catch(err=>{
+            console.log(err);
+        }) 
+});
+
+app.get("/:customListName" , function(req , res){
+    const customListName =  _.capitalize(req.params.customListName);
+
+    List.findOne({ name: customListName })
+          .then(foundList => {
+                if (!foundList) {
+                    // create a new list
+
+                    const list = new List({
+                        name: customListName,
+                        items:defaultItems 
+                    });
+                
+                    list.save();
+                    res.redirect("/" + customListName);
+                } 
+                else {
+                  //show new list
+
+                  res.render("list" , {listTitle: foundList.name , newListItems:foundList.items})
+                }
+            })
+           .catch(err => {
+                console.log(err);
+           });
+
+       
+})
+
+app.post("/" , function(req , res){
+
+   const itemName = req.body.newItem;
+   const listName = req.body.list;
+
+   const item = new Item({
+    name:itemName 
+   });
+
+   if(listName === "Today"){
+      item.save();
+      res.redirect("/");
+   }
+   else{
+    List.findOne({name:listName})
+      .then(foundList=>{
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/" + listName);
+      })
+   }
+   
+});
+
+app.post("/delete", function(req, res) {
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
+  
+    if (listName === "Today") {
+      Item.findByIdAndRemove(checkedItemId)
+        .then(() => {
+          console.log("Successfully deleted checked items.");
+          res.redirect("/");
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).send("Error deleting item.");
+        });
+    } else {
+      List.findOneAndUpdate(
+        { name: listName },
+        { $pull: { items: { _id: checkedItemId } } }
+      )
+        .then(() => {
+          res.redirect("/" + listName);
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).send("Error deleting item from list.");
+        });
+    }
+  });
+  
+
+
+app.get("/about" , function(req, res){
+    res.render("about");
+})
+
+// app.get("/dropdb", function (req, res) {
+//     mongoose.connection.db.dropDatabase()
+//       .then(() => {
+//         console.log("Database dropped");
+//         res.send("Database dropped");
+//       })
+//       .catch(err => {
+//         console.log(err);
+//         res.send("Error dropping database");
+//       });
+//   });
+
+  
+
+app.listen(3000 , function(){
+    console.log("server is running at port 3000");
+});
